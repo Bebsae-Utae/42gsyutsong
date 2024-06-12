7d7d385a4ed5050da44a0361f766cd23f4347241
yutsong <yutsong@student.42gyeongsan.kr>
2024-06-12 16:59:40 +0900

    [pushswap] 1. 기본 파일 구조 생성 2. main 함수 생성 및 필요 함수 정의

---
+++ push_swap/push_swap.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsong <yutsong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 14:05:50 by yutsong           #+#    #+#             */
+/*   Updated: 2024/06/12 16:53:04 by yutsong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*prev;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	int		size;
+	t_node	*top;
+	t_node	*bottom;
+}	t_stack;
+
+void	check_argv(int argc, char **argv);
+
+#endif
---
+++ push_swap/utils_check.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_check.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsong <yutsong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 16:23:41 by yutsong           #+#    #+#             */
+/*   Updated: 2024/06/12 16:30:37 by yutsong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	check_argv(int argc, char **argv)
+{
+	int		idx;
+	char	*data;
+
+	idx = 1;
+	if (argc == 1)
+		print_error();
+	while (idx != argc)
+	{
+		data = argv[idx];
+		while (*data)
+		{
+			if (*data == '-' || *data == '+')
+			{
+				data ++;
+				if (*data < '0' || *data > '9')
+					print_error();
+			}
+			else if (*data != ' ' && (*data < '0' || *data > '9'))
+				print_error();
+			if (*data)
+				data ++;
+		}
+		idx ++;
+	}
+}
