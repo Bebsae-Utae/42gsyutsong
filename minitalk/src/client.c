32cc547c35ddf59464f74421abe57090bb6e31f8
yutsong <yutsong@student.42gyeongsan.kr>
2024-07-09 13:22:18 +0900

    [minitalk] - 클라이언트 조건문 수정

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: yutsong <yutsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 17:45:23 by yutsong           #+#    #+#             */
-/*   Updated: 2024/06/26 14:34:48 by yutsong          ###   ########.fr       */
+/*   Updated: 2024/07/09 13:18:06 by yutsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,14 @@ int	main(int argc, char **argv)
 {
 	int	pid;
 
-	if (argc == 3 && argv[2][0] != '\0')
-	{
-		pid = ft_atoi(argv[1]);
-		if (pid < 100 || pid > 4194304)
-		{
-			ft_putstr_fd("Invalid PID.\n", 1);
-			return (0);
-		}
-		client_send_str(pid, argv[2]);
-	}
-	else
+	if (argc != 3 || argv[2][0] == '\0')
 		ft_putstr_fd("Wrong Arguments.\n", 1);
+	pid = ft_atoi(argv[1]);
+	if (pid < 100 || pid > 4194304)
+	{
+		ft_putstr_fd("Invalid PID.\n", 1);
+		return (0);
+	}
+	client_send_str(pid, argv[2]);
 	return (0);
 }
