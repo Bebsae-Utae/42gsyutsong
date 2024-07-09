a36ab6fc53776b779005f5e16c60cbce146ca3b4
yutsong <yutsong@student.42gyeongsan.kr>
2024-07-09 14:13:54 +0900

    [minitalk] - client.c 오류 예외처리

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: yutsong <yutsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 17:45:23 by yutsong           #+#    #+#             */
-/*   Updated: 2024/07/09 13:18:06 by yutsong          ###   ########.fr       */
+/*   Updated: 2024/07/09 14:13:31 by yutsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	main(int argc, char **argv)
 	int	pid;
 
 	if (argc != 3 || argv[2][0] == '\0')
+	{
 		ft_putstr_fd("Wrong Arguments.\n", 1);
+		return (0);
+	}
 	pid = ft_atoi(argv[1]);
 	if (pid < 100 || pid > 4194304)
 	{
